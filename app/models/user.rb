530ce30562cde8dd8790ab8bae89d31dff07aa86
Kris Mahadhika <krismahadhika@gmail.com>
2023-09-12 21:57:00 +0800

    add username saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 end
